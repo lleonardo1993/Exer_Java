149b1755c56afc1f0454dbbae78fa0a4f3751fef
lleonardo1555@gmail.com <lleonardo1555@gmail.com>
2022-03-09 19:55:53 -0300

    Exercicio, Funções.

---
+++ src/main/java/CourseJava/Vect.java
@@ -0,0 +1,14 @@
+package CourseJava;
+
+public class Vect {
+    public static void main(String[] args) {
+
+        String s = "potato apple lemon";
+
+        String[] vect = s.split(" ");
+
+        System.out.println(vect[0]);
+        System.out.println(vect[1]);
+        System.out.println(vect[2]);
+    }
+}
